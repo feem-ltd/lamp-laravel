beb7dcc01666a1826e9fb159a4c1a51b2fc94668
Shigeo Nakamura <cityhunterplus@gmail.com>
2021-09-19 12:13:01 +0900

    DB のイメージ名を mariadb に変更。 DB のホスト指定が間違っていたので修正。

--- bin/build.bat
+++ bin/build.bat
@@ -2,4 +2,4 @@
 
 docker build --file="build/nginx/Dockerfile" -t feem/lemp-nginx .
 docker build --file="build/laravel/Dockerfile" -t feem/lemp-node-laravel .
-docker build --file="build/mariadb/Dockerfile" -t feem/lemp-mysql .
+docker build --file="build/mariadb/Dockerfile" -t feem/lemp-mariadb .
--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 
 docker build --file="build/nginx/Dockerfile" -t feem/lemp-nginx .
 docker build --file="build/laravel/Dockerfile" -t feem/lemp-node-laravel .
-docker build --file="build/mariadb/Dockerfile" -t feem/lemp-mysql .
+docker build --file="build/mariadb/Dockerfile" -t feem/lemp-mariadb .
 
