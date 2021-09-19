4f6e835baebeb48d43a910babeea0d8ec019a662
Shigeo Nakamura <cityhunterplus@gmail.com>
2021-09-19 17:29:58 +0900

    production 用のビルド設定を追加

--- bin/build-production.bat
+++ bin/build-production.bat
@@ -1,6 +1,6 @@
 @echo off
 
-docker build --file="build/nginx/Dockerfile" -t feem/lamp-nginx .
-docker build --file="build/mariadb/Dockerfile" -t feem/lemp-mysql .
-docker build --file="build/laravel/Dockerfile.production" -t feem/lemp-node-laravel .
-
+docker build --file="build/nginx/Dockerfile" -t feem/lemp-nginx .
+docker build --file="build/laravel/Dockerfile" -t feem/lemp-node-laravel .
+docker build --file="build/laravel/Dockerfile.production" -t feem/lemp-silicense .
+docker build --file="build/mariadb/Dockerfile" -t feem/lemp-mariadb .
---
+++ bin/build.production.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker build --file="build/nginx/Dockerfile" -t feem/lemp-nginx .
+docker build --file="build/laravel/Dockerfile" -t feem/lemp-node-laravel .
+docker build --file="build/laravel/Dockerfile.production" -t feem/lemp-silicense .
+docker build --file="build/mariadb/Dockerfile" -t feem/lemp-mariadb .
