61fcafcd90dd0965dc295c1a04ef868e4c3ecaa6
cityhunterplus <info@shigebox.com>
2021-09-23 01:19:44 +0900

    ファイルコード？

--- bin/build-production.sh
+++ bin/build-production.sh
@@ -4,9 +4,6 @@ echo "Building feem/lamp-node-laravel..."
 docker build --file="build/laravel/Dockerfile" -t feem/lamp-node-laravel .
 echo "\n"
 
-echo "Building feem/lamp-silicense..."
 docker build --file="build/laravel/Dockerfile.production" -t feem/lamp-silicense .
-echo "\n"
 
-echo "Building feem/lamp-mysql..."
 docker build --file="build/mysql/Dockerfile" -t feem/lamp-mysql .
